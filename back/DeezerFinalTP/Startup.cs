5cbe41c9f1b6811c019c534e58e7fc8c0b8b51fa
PusaiKozu <pusaikozu@gmail.com>
2019-11-04 11:57:15 +0100

    Préparation JWT (LoginService)

--- back/DeezerFinalTP/Startup.cs
+++ back/DeezerFinalTP/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using DeezerFinalTP.Tools;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DeezerFinalTP
 {
@@ -24,7 +28,36 @@ namespace DeezerFinalTP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ILoginService, LoginService>();
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSetting>(appSettingsSection);
+            var appSettings = Configuration.Get<AppSetting>();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("secretstring")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +69,7 @@ namespace DeezerFinalTP
             }
 
             app.UseMvc();
+            app.UseCors("AllowAll");
         }
     }
 }
---
+++ back/DeezerFinalTP/Tools/LoginService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeezerFinalTP.Tools
+{
+    public class LoginService : ILoginService
+    {
+        private List<Utilisateur> listeUsers = new List<Utilisateurs>();
+        //{
+        //    //new Utilisateur {Id = 1, FirstName = "Ihab", LastName="ABADI", Email="ihab@utopios.net", Password="123456"}
+        //};
+
+        public Utilisateur LogIn(string email, string password)
+        {
+            Utilisateur utilisateur = listeUsers.SingleOrDefault(x => x.Email == email && x.Password == password);
+            if (utilisateur == null)
+                return null;
+            //Objet pour creer un json web Token
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            //Convertir la chaine de cryptage en bytes
+            byte[] key = Encoding.ASCII.GetBytes("je suis une chaine secret");
+            //Description du contenu du JWT
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor();
+            descriptor.Subject = new ClaimsIdentity(new Claim[]
+            {
+                //Contenu du body du JWT
+                new Claim(ClaimTypes.Email, utilisateur.Email),
+                new Claim(ClaimTypes.NameIdentifier, utilisateur.Id.ToString()),
+                new Claim(ClaimTypes.Name, utilisateur.Nickname),
+            });
+            //date d'expiration du JWT
+            descriptor.Expires = DateTime.Now.AddDays(1);
+            //Algo pour signier le JWT
+            descriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            //Création du token
+            SecurityToken token = tokenHandler.CreateToken(descriptor);
+            //Convertir le token en chaine de caractère et le stocker dans le Token d'User
+            utilisateur.Token = tokenHandler.WriteToken(token);
+            return utilisateur;
+        }
+    }
+
+    public interface ILoginService
+    {
+    }
+}
