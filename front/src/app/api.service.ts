ca6a3c4095f35513a5db9516be0e77046f39c728
Sam <samira.boudiaf59@gmail.com>
2019-11-04 13:33:02 +0100

    modif v1

--- front/src/app/api.service.ts
+++ front/src/app/api.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 @Injectable()
 export class ApiService {
 
   urlBase = "http://localhost:59713";
-  constructor(private http : HttpClient) { 
+  constructor(private http: HttpClient) {
 
   }
 
@@ -13,10 +13,10 @@ export class ApiService {
   }
 
   postApi = (url, data) => {
-    return this.http.post(this.urlBase + url,data);
+    return this.http.post(this.urlBase + url, data);
   }
 
-  upload = (url, formdata)=> {
+  upload = (url, formdata) => {
     //pour envoyer une requete vers une api protegée par un jwt, il faut ajouter dans l'entete de la requete l'authorization avec le token
     const headers = new HttpHeaders({
       /******pas mettre car affiche erreur */
@@ -24,18 +24,16 @@ export class ApiService {
       // "Authorization": "Bearer "+localStorage.getItem("token")
     });
     //reportProgress => pour ecouter la progression de l'upload; observe => pour ecouter la totalité de l'event et non uniquement la réponse du serveur
-    return this.http.post(this.urlBase+'/'+url, formdata,{headers : headers,reportProgress: true, observe:'events'});
+    return this.http.post(this.urlBase + '/' + url, formdata, { headers: headers, reportProgress: true, observe: 'events' });
   }
 
   put = (url, data) => {
     //pour envoyer le token, on spécifie que c'est en json avec content-type 
     const headers = new HttpHeaders({
-    //   "Content-type": "application/json",
-    //   "Authorization": "Bearer " + localStorage.getItem("token")
+      //   "Content-type": "application/json",
+      //   "Authorization": "Bearer " + localStorage.getItem("token")
     });
-    return this.http.put(this.urlBase + '/' + url, data, {headers : headers});
+    return this.http.put(this.urlBase + '/' + url, data, { headers: headers });
     // return this.http.put(this.baseUrl + '/' + url, data);
-
   }
-
-  }
+}
